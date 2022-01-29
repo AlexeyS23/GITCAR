16376f9975d40a3d2e9ed245479a44e3ed23b655
unknown <alexsitalo1983@gmail.com>
2022-01-29 15:26:20 +0200

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
 
     CarBasic car=new CarBasic();
-    car.color="";
-    car.door="";
-    car.engine="";
-    car.number="";
+    car.color="red";
+    car.engine="7999c";
+    car.door="2";
+    car.number="119018";
 
+        System.out.println(car.color);
 }
 }
