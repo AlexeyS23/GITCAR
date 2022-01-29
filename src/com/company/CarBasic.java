af5d6102f7401e21d3114211d1967468563219bf
unknown <alexsitalo1983@gmail.com>
2022-01-29 14:37:57 +0200

    The basic structure of class is ready without any features

---
+++ src/com/company/CarBasic.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class CarBasic {
+
+    String color;
+    String engine;
+    String door;
+    String number;
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    //the basic structure
+    public static void main(String[] args) {
+
+
+    CarBasic car=new CarBasic();
+    car.color="";
+    car.door="";
+    car.engine="";
+    car.number="";
+
+}
+}
